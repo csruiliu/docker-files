bed84483831287adfd681899c4058c29aa613741
csruiliu <rui.liu@proton.me>
2021-12-30 11:15:09 -0600

    update scripts for agg incremental

--- ioqp-compile/deploy-jars.sh
+++ ioqp-compile/deploy-jars.sh
@@ -10,3 +10,9 @@ cp $HOME/Develop/ioqp/sql/core/target/scala-2.11/spark-sql_2.11-2.4.0.jar $HOME/
 cp $HOME/Develop/ioqp/sql/catalyst/target/scala-2.11/spark-catalyst_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
 cp $HOME/Develop/ioqp/external/avro/target/scala-2.11/spark-avro_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
 cp $HOME/Develop/ioqp/external/kafka-0-10-sql/target/scala-2.11/spark-sql-kafka-0-10_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
+
+docker image rm csruiliu/ioqp-load:latest
+docker image rm csruiliu/ioqp-process:latest
+
+docker build -t csruiliu/ioqp-load /home/ruiliu/Develop/docker-files/ioqp-load
+docker build -t csruiliu/ioqp-process /home/ruiliu/Develop/docker-files/ioqp-process
