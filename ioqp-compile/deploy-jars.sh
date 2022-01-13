67f9840b7ab806b02d71d22a6c29552aa39deed0
csruiliu <rui.liu@proton.me>
2022-01-12 18:52:58 -0600

    update scripts

--- ioqp-compile/deploy-jars.sh
+++ ioqp-compile/deploy-jars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp $HOME/Develop/ioqp/relaqs/target/scala-2.11/ruiliu-relaqs_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-load/jars/
+cp $HOME/Develop/ioqp/aqp/target/scala-2.11/ruiliu-aqp_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-load/jars/
 cp $HOME/Develop/ioqp/sql/catalyst/target/scala-2.11/spark-catalyst_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-load/jars/
 cp $HOME/Develop/ioqp/external/avro/target/scala-2.11/spark-avro_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-load/jars/
 cp $HOME/Develop/ioqp/external/kafka-0-10-sql/target/scala-2.11/spark-sql-kafka-0-10_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-load/jars/
 
-cp $HOME/Develop/ioqp/relaqs/target/scala-2.11/ruiliu-relaqs_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
+cp $HOME/Develop/ioqp/aqp/target/scala-2.11/ruiliu-aqp_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
 cp $HOME/Develop/ioqp/sql/core/target/scala-2.11/spark-sql_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
 cp $HOME/Develop/ioqp/sql/catalyst/target/scala-2.11/spark-catalyst_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
 cp $HOME/Develop/ioqp/external/avro/target/scala-2.11/spark-avro_2.11-2.4.0.jar $HOME/Develop/docker-files/ioqp-process/jars/
