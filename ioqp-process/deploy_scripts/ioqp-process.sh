b917f27765f6828bb58bbcd17c0052dd6bd81519
csruiliu <rui.liu@gmx.com>
2022-01-15 17:17:16 -0600

    update ioqp-process script

--- ioqp-process/deploy_scripts/ioqp-process.sh
+++ ioqp-process/deploy_scripts/ioqp-process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 /etc/init.d/ssh start
+sleep 2
+/home/deploy_scripts/setup-password.sh
 sleep 1
-./setup-password.sh
-sleep 1
-./setup-spark.sh
+/home/deploy_scripts/setup-spark.sh
 sleep 1
 /home/run_scripts/run_tpch_test.sh
