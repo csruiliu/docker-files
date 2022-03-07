09a420bc6d35156e034bdfd48fb915459188d4b4
csruiliu <rui.liu@gmx.com>
2022-03-07 10:15:55 -0600

    update docker and script file

--- rotary-aqp-load/deploy_script/create_topic.sh
+++ rotary-aqp-load/deploy_script/create_topic.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
- 
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Supplier --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Part --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic PartSupp --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Customer --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Orders --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Lineitem --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Nation --partitions 20 --replication-factor 1
-kafka-topics.sh -zookeeper localhost:2181 --create --topic Region --partitions 20 --replication-factor 1
+
+PARTITION=20
+
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Supplier --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Part --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic PartSupp --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Customer --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Orders --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Lineitem --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Nation --partitions $PARTITION --replication-factor 1
+kafka-topics.sh -zookeeper localhost:2181 --create --topic Region --partitions $PARTITION --replication-factor 1
